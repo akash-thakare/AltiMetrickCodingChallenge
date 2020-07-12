9427f531bac5ff0f3e0a4992ac70c7b6fa60b52e
thakarea <akash.thakarea@gmail.com>
2020-07-12 18:47:25 +0530

    Revert "test"
    This reverts commit 9ded1dd1acdd2f1beae98ac2b715d5d1e1631046.

--- src/main/java/com/altimetrik/springApiJWtAuthentication.java
+++ src/main/java/com/altimetrik/springApiJWtAuthentication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class springApiJWtAuthentication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(springApiJWtAuthentication.class, args);
 	}
