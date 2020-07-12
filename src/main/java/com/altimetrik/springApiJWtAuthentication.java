9ded1dd1acdd2f1beae98ac2b715d5d1e1631046
thakarea <akash.thakarea@gmail.com>
2020-07-12 18:43:13 +0530

    test

--- src/main/java/com/altimetrik/springApiJWtAuthentication.java
+++ src/main/java/com/altimetrik/springApiJWtAuthentication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class springApiJWtAuthentication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(springApiJWtAuthentication.class, args);
 	}
